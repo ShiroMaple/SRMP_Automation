0a32237de4768458446e753e4b37c72d876de84e
ShiroMaple <shiromaple@gmail.com>
2025-07-17 17:02:46 +0800

    weiboST

--- agent/my_action.py
+++ agent/my_action.py
@@ -2,16 +2,33 @@ from maa.agent.agent_server import AgentServer
 from maa.custom_action import CustomAction
 from maa.context import Context
 
-
-@AgentServer.custom_action("my_action_111")
-class MyCustomAction(CustomAction):
+#运行节点
+"""
+@AgentServer.custom_action("run")
+class Run(CustomAction):
 
     def run(
         self,
         context: Context,
         argv: CustomAction.RunArg,
-    ) -> bool:
+    ) -> CustomAction.RunResult | bool:
+            try:
+                args =parse_query_args(argv)
+                type=args.get("type,"task")
+                key=args.get("key","")
 
+                if type=="" or key=="":
+                return False
+
+                if type=="task":
+                context.run_task(key)
+                elif type=="node":
+                context.run_action(key)
+
+                return True
+            except Exception as e:
+                return Prompt.error("运行节点",e)
         print("my_action_111 is running!")
 
         return True
+"""
