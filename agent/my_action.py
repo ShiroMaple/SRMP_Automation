8edd06ea059c6b1541fb1a67f14abdff38e91d65
ShiroMaple <shiromaple@gmail.com>
2025-07-30 16:55:50 +0800

    try using Severchan with Custom

--- agent/my_action.py
+++ agent/my_action.py
@@ -2,7 +2,7 @@ from maa.agent.agent_server import AgentServer
 from maa.custom_action import CustomAction
 from maa.context import Context
 import json
-
+from serverchan_sdk import sc_send
 
 @AgentServer.custom_action("my_action_111")
 class MyCustomAction(CustomAction):
@@ -21,15 +21,17 @@ class MyCustomAction(CustomAction):
         self,
         context: Context,
         argv: CustomAction.RunArg,
-    ) -> bool:
-        # 1. 解析 custom_action_param (JSON 字符串 -> Python 字典)
-        try:
-            params = json.loads(argv.custom_action_param)  # 解析 JSON
-            srmp = params.get("srmp")  # 获取 srmp 参数
-        except json.JSONDecodeError:
-            print("Error: custom_action_param is not valid JSON!")
-            return False
+    ) -> bool:        
 
-        # 2. 打印 srmp 的值
-        print(f"Received parameter : {srmp}")
+        params = json.loads(argv.custom_action_param)  # 解析 JSON        
+        
+        #print(f"argv.custom_action_param : {argv.custom_action_param}") #str型字符串
+        #print(f"Transfer to JSON : {params}")   # Python 字典
+        print(f"Result : {params.get("srmp")}")  # 获取 srmp 参数并打印
+        sendkey="sctp2102ta-lbduuk43fh2pz462ln61oko4"
+        title="from MaaFw"
+        desp="This is a test message from MaaFw\n\n" + params.get("srmp", "where is srmp?")  # 获取 srmp 参数，若不存在则使用默认值
+        options={"tags":"MaaFw"}
+        response = sc_send(sendkey,title,desp,options)  # 调用 serverchan_sdk 的 sc_send 函数发送消息
+        print(response)  # 打印发送结果
         return True  
